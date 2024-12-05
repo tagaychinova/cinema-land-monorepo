b7db24a3c4b03abb77a028fabf5ca6fd2c8947ac
Anna Tagaychinova <tagaychinova@gmail.com>
2024-12-05 13:30:41 +0300

    prisma

--- apps/cinema-land.admin/src/app/api/movie/route.ts
+++ apps/cinema-land.admin/src/app/api/movie/route.ts
@@ -1,6 +1,6 @@
 import { unstable_noStore as noStore } from 'next/cache';
 
-import prisma from '../../../lib/prisma';
+import prisma from '@cinema-land-monorepo/db';
 
 export async function GET() {
   noStore();
