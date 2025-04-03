996f931d65538240e68690bd198b0b71f04b52b3
Anna Tagaychinova <tagaychinova@gmail.com>
2025-04-03 12:42:26 +0300

    set unoptimized images

--- apps/cinema-land.site/next.config.js
+++ apps/cinema-land.site/next.config.js
@@ -17,6 +17,10 @@ const nextConfig = {
     // For other options, see https://styled-components.com/docs/tooling#babel-plugin
     styledComponents: true,
   },
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 const plugins = [
