fac267b334c58847c09de1e1c5c5adc1428fd492
Anna Tagaychinova <tagaychinova@gmail.com>
2025-04-04 14:32:36 +0300

    nx

--- apps/cinema-land.site/next.config.ts
+++ apps/cinema-land.site/next.config.ts
@@ -1,14 +1,7 @@
-import { composePlugins, withNx } from '@nx/next';
-import type { WithNxOptions } from '@nx/next/plugins/with-nx';
+import type { NextConfig } from 'next';
 
-const nextConfig: WithNxOptions = {
-  nx: {},
-
-  compiler: {
-    styledComponents: true,
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-const plugins = [withNx];
-
-module.exports = composePlugins(...plugins)(nextConfig);
+export default nextConfig;
