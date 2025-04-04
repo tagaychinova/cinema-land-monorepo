387be3dddfb65cd3661b2e6718c045b8350b975b
Anna Tagaychinova <tagaychinova@gmail.com>
2025-04-04 15:36:45 +0300

    test

--- apps/my-new-app/next.config.js
+++ apps/my-new-app/next.config.js
@@ -1,27 +1,7 @@
-//@ts-check
+import type { NextConfig } from "next";
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
-
-/**
- * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
- **/
-const nextConfig = {
-  nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
-
-  compiler: {
-    // For other options, see https://styled-components.com/docs/tooling#babel-plugin
-    styledComponents: true,
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
-module.exports = composePlugins(...plugins)(nextConfig);
+export default nextConfig;
