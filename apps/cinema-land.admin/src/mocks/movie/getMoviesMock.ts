a96da80717b98d7f2851b544ad801c53e9408286
Anna Tagaychinova <tagaychinova@gmail.com>
2025-06-04 14:32:05 +0300

    fix build

--- apps/cinema-land.admin/src/mocks/movie/getMoviesMock.ts
+++ apps/cinema-land.admin/src/mocks/movie/getMoviesMock.ts
@@ -5,20 +5,20 @@ const movies: Movie[] = [
   {
     id: '1',
     title: '222',
-    genre: 'Фэнтези',
+    genre: ['Фэнтези'],
     yearOfIssue: 2001,
   },
-  { id: '2', title: '333', genre: 'Cersei', yearOfIssue: 42 },
-  { id: '3', title: 'Lannister', genre: 'Jaime', yearOfIssue: 45 },
-  { id: '4', title: 'Stark', genre: 'Arya', yearOfIssue: 16 },
-  { id: '5', title: 'Targaryen', genre: 'Daenerys', yearOfIssue: 5 },
-  { id: '6', title: 'Melisandre', genre: 'qqq', yearOfIssue: 150 },
-  { id: '7', title: 'Clifford', genre: 'Ferrara', yearOfIssue: 44 },
-  { id: '8', title: 'Frances', genre: 'Rossini', yearOfIssue: 36 },
-  { id: '9', title: 'Roxie', genre: 'Harvey', yearOfIssue: 65 },
+  { id: '2', title: '333', genre: ['Cersei'], yearOfIssue: 42 },
+  { id: '3', title: 'Lannister', genre: ['Jaime'], yearOfIssue: 45 },
+  { id: '4', title: 'Stark', genre: ['Arya'], yearOfIssue: 16 },
+  { id: '5', title: 'Targaryen', genre: ['Daenerys'], yearOfIssue: 5 },
+  { id: '6', title: 'Melisandre', genre: ['qqq'], yearOfIssue: 150 },
+  { id: '7', title: 'Clifford', genre: ['Ferrara'], yearOfIssue: 44 },
+  { id: '8', title: 'Frances', genre: ['Rossini'], yearOfIssue: 36 },
+  { id: '9', title: 'Roxie', genre: ['Harvey'], yearOfIssue: 65 },
 ];
 
 export const getMoviesMock = http.get(
   `${process.env.NEXT_RUNTIME}/api/movie`,
-  () => HttpResponse.json(movies)
+  () => HttpResponse.json(movies),
 );
